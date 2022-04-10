c3be2cb09e52ab43c9f1967be056815e0d9e2b75
damien54000 <98956920+damien54000@users.noreply.github.com>
2022-04-10 19:04:13 +0200

    html + css

---
+++ javascript/vote.js
@@ -0,0 +1,9 @@
+function popup1() {
+  var popup1 = document.getElementById("popup");
+  popup1.classList.toggle("show");
+}
+
+function popup2() {
+  var popup2 = document.getElementById("popupbis");
+  popup2.classList.toggle("show");
+}
