d0b73195c460b4a5a20e678dcbb671e356f56605
Hevie <okabrit@gmail.com>
2022-05-05 14:33:42 +0200

    Update validateModifier.php

--- validateModifier.php
+++ validateModifier.php
@@ -3,32 +3,27 @@
 
   $nom='';
   $prenom='';
-  $id='';
+  $id=$_SESSION['id'];
   $email='';
   $password='';
   $newPassword='';
   $error_modification='';
 
+
   if(empty($_POST["nom2"])){
-    $error_modification.="Veuillez entrer votre nom";
+    $nom = getLastName($connection, $id);
   }else{
     $nom=$_POST["nom2"];
   }
 
   if(empty($_POST["prenom2"])){
-    $error_modification.="Veuillez entrer votre prénom";
+    $prenom = getName($connection, $id);
   }else{
     $prenom=$_POST["prenom2"];
   }
 
-  if(empty($_POST["identifier3"])){
-    $error_modification.="Veuillez entrer votre identifiant";
-  }else{
-    $id=$_POST["identifier3"];
-  }
-
   if(empty($_POST["email2"])){
-    $error_modification.="Veuillez entrer votre adresse mail";
+    $email = getMail($connection, $id);
   }else{
     $email=$_POST["email2"];
   }
@@ -49,23 +44,41 @@
     $error_modification.="Veuillez confirmer votre nouveau mot de passe";
   }
 
-  if(checkUserName($connection, $id)){
-    $error_modification.='Cet identifiant n\'existe pas';
-  }else if( checkPassword($connection,$password,$id)["mdp"] == password_hash($password1, PASSWORD_DEFAULT); ){
+  if(checkPassword($connection,$password,$id)["mdp"] == password_hash($password1, PASSWORD_DEFAULT)){
     modifPassword($connection, $newPassword, $id);
   }else{
     $error_modification.='Mot de passe invalide';
   }
 
-  function checkUserName($connection, $username) {
-     $query = "SELECT COUNT(*) AS count FROM users WHERE id=:username";
-     $statement = $connection->prepare($query);
-     $statement->bindValue(":username", $username, PDO::PARAM_STR);
-     $statement->execute();
+  function getMail($connection, $id) {
+    $query = "SELECT mail FROM users WHERE id=:id";
+    $statement = $connection->prepare($query);
+    $statement->bindValue(":id", $id, PDO::PARAM_STR);
+    $statement->execute();
 
-     $row = $statement->fetch(PDO::FETCH_ASSOC);
-     return $row["count"] == "0";
-   }
+    $row = $statement->fetch(PDO::FETCH_ASSOC);
+    return $row["mail"];
+  }  
+
+  function getName($connection, $id) {
+    $query = "SELECT prenom FROM users WHERE id=:id";
+    $statement = $connection->prepare($query);
+    $statement->bindValue(":id", $id, PDO::PARAM_STR);
+    $statement->execute();
+
+    $row = $statement->fetch(PDO::FETCH_ASSOC);
+    return $row["prenom"];
+  }  
+
+  function getLastName($connection, $id) {
+    $query = "SELECT nom FROM users WHERE id=:id";
+    $statement = $connection->prepare($query);
+    $statement->bindValue(":id", $id, PDO::PARAM_STR);
+    $statement->execute();
+
+    $row = $statement->fetch(PDO::FETCH_ASSOC);
+    return $row["nom"];
+  }
 
   function checkPassword($connection, $password, $id){
     $query='SELECT mdp FROM users WHERE id=:id';
